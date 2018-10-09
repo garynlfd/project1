f9c2d2eb47cb53500e806ed731486f3b0f1f4152
garynlfd <garyskyking0908@gmail.com>
2018-10-09 11:36:58 +0800

    second commit

---
+++ project1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    int a_row = 0, a_col = 0;
+    int max = 0;
+    cin >> row >> col;
+    int map[row][col];
+    auto start = chrono::high_resolution_clock::now();
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            cin >> map[i][j];
+            if(i == 0 && j == 0)
+            {
+                max = map[i][j];
+                a_row = i, a_col = j;
+            }
+            else
+            {
+                if(map[i][j] > max)
+                {
+                    max = map[i][j];
+                    a_row = i, a_col = j;
+                }
+            }
+        }
+    }
+    auto end = chrono::high_resolution_clock::now();
+    cout << max << endl;
+    chrono::duration<double, nano> diff = end - start;
+    cout << a_row << ' ' << a_col << endl;
+    cout << diff.count() << " nanoseconds" << std::endl;
+    return 0;
+}
