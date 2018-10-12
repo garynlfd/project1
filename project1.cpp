8d211fdf3b99bcecd72f40b341fdbeec934cff50
garynlfd <garyskyking0908@gmail.com>
2018-10-12 16:35:02 +0800

    sixth commit

--- project1.cpp
+++ project1.cpp
@@ -10,10 +10,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int row, col;
-    int matrix[row][col];
     int num = 0;
-    int tmp[1000][1000] = {0};
-    int ans[1000][1000] = {0};
     string file = "./";
     string studentID = argv[1];
     string fileinput = file + studentID + "/matrix.data";
@@ -23,9 +20,12 @@ int main(int argc, char *argv[])
     input.open(fileinput.c_str());
     if(input)
     {
+        input >> row >> col;
+        int matrix[row][col];
+        int tmp[row][col] = {0};
+        int ans[row][col] = {0};
         while(!input.eof())
         {
-            input >> row >> col;
             for(int i = 0; i < row; ++i)
             {
                 for(int j = 0; j < col; ++j)
@@ -36,93 +36,93 @@ int main(int argc, char *argv[])
             int eat;
             input >> eat;
         }
-    }
-    input.close();
-    auto start = chrono::high_resolution_clock::now();
-    for(int i = 0; i < row; ++i)
-    {
-        for(int j = 0; j < col; ++j)
+        input.close();
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < row; ++i)
         {
-            if(j == 0)
+            for(int j = 0; j < col; ++j)
             {
-                if(matrix[i][j] >= matrix[i][j+1])
+                if(j == 0)
                 {
-                    tmp[i][j] = 1;
-                    if(matrix[i][j] > matrix[i][j+1])
+                    if(matrix[i][j] >= matrix[i][j+1])
                     {
-                        ++j;
+                        tmp[i][j] = 1;
+                        if(matrix[i][j] > matrix[i][j+1])
+                        {
+                            ++j;
+                        }
                     }
                 }
-            }
-            else if(j == col - 1)
-            {
-                if(matrix[i][j] >= matrix[i][j-1])
+                else if(j == col - 1)
                 {
-                    tmp[i][j] = 1;
-                }
-            }
-            else
-            {
-                if(matrix[i][j] >= matrix[i][j+1] && matrix[i][j] >= matrix[i][j-1])
-                {
-                    tmp[i][j] = 1;
-                    if(matrix[i][j] > matrix[i][j+1])
+                    if(matrix[i][j] >= matrix[i][j-1])
                     {
-                        ++j;
-                    }
-                }
-            }
-        }
-    }
-    for(int i = 0; i < row; ++i)
-    {
-        for(int j = 0; j < col; ++j)
-        {
-            if(tmp[i][j] == 1)
-            {
-                if(i == 0)
-                {
-                    if(matrix[i][j] >= matrix[i+1][j])
-                    {
-                        ans[i][j] = 1;
-                        num++;
-                    }
-                }
-                else if(i == row - 1)
-                {
-                    if(matrix[i][j] >= matrix[i-1][j])
-                    {
-                        ans[i][j] = 1;
-                        num++;
+                        tmp[i][j] = 1;
                     }
                 }
                 else
                 {
-                    if(matrix[i][j] >= matrix[i-1][j] && matrix[i][j] >= matrix[i+1][j])
+                    if(matrix[i][j] >= matrix[i][j+1] && matrix[i][j] >= matrix[i][j-1])
                     {
-                        ans[i][j] = 1;
-                        num++;
+                        tmp[i][j] = 1;
+                        if(matrix[i][j] > matrix[i][j+1])
+                        {
+                            ++j;
+                        }
                     }
                 }
             }
         }
-    }
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<double, nano> diff = end - start;
-    cout << num << endl;
-    for(int i = 0; i < row; ++i)
-    {
-        for(int j = 0; j < col; ++j)
+        for(int i = 0; i < row; ++i)
         {
-            if(ans[i][j] == 1)
+            for(int j = 0; j < col; ++j)
             {
-                cout << i << " " << j << endl;
+                if(tmp[i][j] == 1)
+                {
+                    if(i == 0)
+                    {
+                        if(matrix[i][j] >= matrix[i+1][j])
+                        {
+                            ans[i][j] = 1;
+                            num++;
+                        }
+                    }
+                    else if(i == row - 1)
+                    {
+                        if(matrix[i][j] >= matrix[i-1][j])
+                        {
+                            ans[i][j] = 1;
+                            num++;
+                        }
+                    }
+                    else
+                    {
+                        if(matrix[i][j] >= matrix[i-1][j] && matrix[i][j] >= matrix[i+1][j])
+                        {
+                            ans[i][j] = 1;
+                            num++;
+                        }
+                    }
+                }
             }
         }
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double, nano> diff = end - start;
+        cout << num << endl;
+        for(int i = 0; i < row; ++i)
+        {
+            for(int j = 0; j < col; ++j)
+            {
+                if(ans[i][j] == 1)
+                {
+                    cout << i << " " << j << endl;
+                }
+            }
+        }
+        cout << diff.count() << " nanoseconds" << std::endl;
+        output.open(fileoutput.c_str());
+        //output <<
+        output.close();
     }
-    cout << diff.count() << " nanoseconds" << std::endl;
-    output.open(fileoutput.c_str());
-    //output <<
-    output.close();
     return 0;
 }
