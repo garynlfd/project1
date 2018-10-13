327069e2584dc9d77abe04887f8fc20c4e4333f2
garynlfd <garyskyking0908@gmail.com>
2018-10-13 19:13:41 +0800

    ninth commit

--- project1.cpp
+++ project1.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
         input >> row >> col;
         int matrix[row][col];
         int tmp[row][col] = {0};
-        int ans[row*col] = {0};
+        int ans[row*col*2] = {0};
         while(!input.eof())
         {
             for(int i = 0; i < row; ++i)
